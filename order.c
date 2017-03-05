0d2cc78103ec4f946e41d56b0e08b80f91387e88
Austin <pachano.austin@gmail.com>
2017-03-04 16:23:26 -0800

    updates

--- order.c
+++ order.c
@@ -1,10 +1,66 @@
-#include "pos.h"
+#include "order.h"
 
-struct order *neworder()
+Order initiateorder(int store, int regi, int uid)
 {
-  struct order *new;
-  new = malloc(sizeof(struct order));
-  order->regi = 3;
-  order->date = 20000203;
-  order->inittime = 1134;
+  Order new = malloc(sizeof(struct order));
+  new->regi = regi;
+  new->uid = uid;
+  new->store = store;
+  new->item = NULL;
+  return new;
+}
+
+// ----------- Code For File I/O ---------- //
+
+int storeorder(Order);
+{
+  fp = fopen("/orders/que.text", "r+");
+  fgets(buffer,120,fp);
+  fprintf(fp, %i %i", store, regi);
+
+  fclose(fp);
+}
+
+// ----- Code For Order Manipulation -----  //
+Item finditem
+
+
+int additem(Order order, int itemid, int quantity)
+{
+  Item new = malloc(sizeof(struct item));
+  new->itemid = itemid;
+  new->quantity = quantity;
+  new->next = order->next;
+  order->next = new;
+}
+int moditem(Order order, int itemid, int quantity)
+{
+  
+}
+int voiditem(Order order,  int itemid)
+{
+  if (order->item->itemid == itemid)
+  {
+    Item temp = order->item;
+    order->item = order->item->next;
+    free(temp);
+  }
+  else
+  {
+    voidrecursion(item, itemid);
+  }
+  return 1;
+}
+int voidrecursion(Item item, int itemid)
+{
+  if (item->next->itemid == itemid)
+  {
+    Item temp = item->next;
+    Item->next = item->next->next;
+    free(temp);
+  }
+  else
+  {
+    voidrecursion(item, itemid);
+  }
 }
---
+++ order.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <time.h>
+
+typedef struct timestamp* Timestamp;
+
+struct timestamp{ //structure that stores time stamp information for an order
+  int date;
+  int timeinit;
+  int timestore;
+  int timepay;
+  int timecomp;
+  int timepresent;
+}
+
+typedef struct order* Order;
+
+struct order{ //structure to store information on an order
+  Timestamp timestamp;
+  int store;
+  int regi;
+  int uid;
+  Item item;
+}
+
+typedef struct item Item;
+
+struct item{ //Structure to store information on an item
+  int itemid;
+  int quantity;
+  Item next;
+}
+
+//Begin function prototyping
+// ----- Register Initiation -----
+int initateregister(); //Initiates variables for register use
+
+int initiatemenu(); //initiates menu data
+
+// ------- Order functions -------
+Order initiateorder(int store, int regi, int uid); //Creates a new order
+
+int storeorder(Order); //Stores an order in the order que
+Order loadorder(); //Loads an order from order que
+int comporder(Order); //Marks order as complete and removes from order que
+//Item manipulation
+int additem(Order, int itemid, int quantity);
+int removeitem(Order order, int itemid, int quantity);
+int voiditem(Order order, int itemid);
+//Supplimental functions
+int timestamp();
